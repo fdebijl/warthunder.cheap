7ebf7754e372378d88ece9265874512e510bc65e
Floris de Bijl <floris.debijl@gmail.com>
2024-12-30 15:59:12 +0100

    fix: incomplete email footer

--- shared/src/mailfactory/templates.ts
+++ shared/src/mailfactory/templates.ts
@@ -2,7 +2,7 @@ import { EventType } from '../domain/index.js';
 
 const HUMAN_READABLE_EVENT_TYPES = {
   'priceChange': 'a one-time discount alert for this item',
-  'itemAvailable': 'a one-itme availability alert for this item',
+  'itemAvailable': 'a one-time availability alert for this item',
   'newItem': 'alerts for new items in the store'
 }
 
@@ -20,13 +20,13 @@ export const generateFooter = ({ eventType, format }: { eventType?: EventType, f
     if (format === 'text') {
       return 'Greetings,\r\nFloris\r\nWarthunder.cheap\n\n' +
       `You are receiving this email because your email address was signed up to receive ${HUMAN_READABLE_EVENT_TYPES[eventType]}.\n` +
-      eventType === 'newItem' ? '' : 'You will not receive any further alerts for this item unless you subscribe again on https://warthunder.cheap/">.\n' +
+      (eventType === 'newItem' ? '' : 'You will not receive any further alerts for this item unless you subscribe again on https://warthunder.cheap/">.\n') +
       'Should you wish to never receive any alerts again, simply click the unsubscribe link below.';
     } else if (format === 'html') {
       return '<p>Greetings,<br>Floris<br>Warthunder.cheap</p>' +
       '<br><p style="font-size: 10pt; color: #797979;">' +
         `You are receiving this email because your email address was signed up to receive ${HUMAN_READABLE_EVENT_TYPES[eventType]}.\n` +
-        eventType === 'newItem' ? '' : 'You will not receive any further alerts for this item unless you subscribe again on <a href="https://warthunder.cheap/" target="_blank">warthunder.cheap</a>.\n' +
+        (eventType === 'newItem' ? '' : 'You will not receive any further alerts for this item unless you subscribe again on <a href="https://warthunder.cheap/" target="_blank">warthunder.cheap</a>.\n') +
         'Should you wish to never receive any alerts again, simply click the unsubscribe link below.' +
       '</p>';
     }
