40951a63da87a7e278ba9ae304277ef1e4b96d18
Fdebijl <floris@feedbackfruits.com>
2025-01-04 01:53:46 +0100

    fix: insertPrice not working reliably

--- shared/src/db/prices/insert.ts
+++ shared/src/db/prices/insert.ts
@@ -1,8 +1,9 @@
+import { ObjectId } from 'mongodb';
 import { connect } from '../connect.js';
 import { Price } from '../../domain/index.js';
 import { queryPrices } from './query.js';
 
-export const insertPrice = async (price: Price): Promise<void> => {
+export const insertPrice = async (price: Price): Promise<ObjectId | void> => {
   const db = await connect();
 
   const countForDate = await queryPrices({ itemId: price.itemId, date: price.date });
@@ -11,5 +12,6 @@ export const insertPrice = async (price: Price): Promise<void> => {
     return;
   }
 
-  await db.collection('prices').insertOne(price);
+  const result = await db.collection('prices').insertOne(price);
+  return result.insertedId;
 }
